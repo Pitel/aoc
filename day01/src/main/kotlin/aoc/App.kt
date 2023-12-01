7fcbbdc7382a9b17615ef66858d2912a2f409359
Ing. Jan Kaláb <jan@kalabovi.org>
2023-12-01 17:06:23 +0100

    Better

--- day01/src/main/kotlin/aoc/App.kt
+++ day01/src/main/kotlin/aoc/App.kt
@@ -6,45 +6,23 @@ fun main() {
         .lineSequence()
         .filterNot { it.isBlank() }
 
-    println(
-        input
-            .map { l -> l.mapNotNull { c -> c.digitToIntOrNull() } }
-            .sumOf { 10 * it.first() + it.last() }
-    )
+    fun Sequence<String>.magic() =
+        map { l -> l.mapNotNull { c -> c.digitToIntOrNull() } }.sumOf { 10 * it.first() + it.last() }
+
+    println(input.magic())
 
     println(
-        input.sumOf { l ->
-            val first = l.fold("") { acc, c ->
-                (acc + c)
-                    .replaceFirst("one", "1")
-                    .replaceFirst("two", "2")
-                    .replaceFirst("three", "3")
-                    .replaceFirst("four", "4")
-                    .replaceFirst("five", "5")
-                    .replaceFirst("six", "6")
-                    .replaceFirst("seven", "7")
-                    .replaceFirst("eight", "8")
-                    .replaceFirst("nine", "9")
-            }
-                .first { it.isDigit() }
-                .digitToInt()
-
-            val last = l.reversed().fold("") { acc, c ->
-                (acc + c)
-                    .replaceFirst("one".reversed(), "1")
-                    .replaceFirst("two".reversed(), "2")
-                    .replaceFirst("three".reversed(), "3")
-                    .replaceFirst("four".reversed(), "4")
-                    .replaceFirst("five".reversed(), "5")
-                    .replaceFirst("six".reversed(), "6")
-                    .replaceFirst("seven".reversed(), "7")
-                    .replaceFirst("eight".reversed(), "8")
-                    .replaceFirst("nine".reversed(), "9")
-            }
-                .first { it.isDigit() }
-                .digitToInt()
-
-            10 * first + last
-        }
+        input.map {
+            it
+                .replace("one", "o1e")
+                .replace("two", "t2o")
+                .replace("three", "t3e")
+                .replace("four", "f4r")
+                .replace("five", "f5e")
+                .replace("six", "s6x")
+                .replace("seven", "s7n")
+                .replace("eight", "e8t")
+                .replace("nine", "n9e")
+        }.magic()
     )
 }
